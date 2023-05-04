40a1b073624558d608b5d26b758266c1f49e274e
Maduki-tech <maduki.business@gmail.com>
2023-05-04 15:16:52 +0200

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,27 @@
-import { type AppType } from "next/dist/shared/lib/utils";
+import { type AppType } from 'next/dist/shared/lib/utils';
+import Script from 'next/script';
 
-import "~/styles/globals.css";
+import '~/styles/globals.css';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+    return (
+        <>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-2BQ316HYBQ"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-2BQ316HYBQ');
+        `}
+            </Script>
+            <Component {...pageProps} />
+        </>
+    );
 };
 
 export default MyApp;
